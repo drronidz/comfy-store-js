32617c9e649f26c5b9b09614d85f0e68e7e01c5f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-28 22:54:19 +0100

    Comfy Store Project (John Smigla): Setting up Sidebar toggle

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,17 @@
 import { getElement } from './utils.js';
+
+const toggleNav = getElement('.toggle-nav')
+const sidebarOverlay = getElement('.sidebar-overlay')
+const closeBtn = getElement('.sidebar-close')
+
+toggleNav.addEventListener('click', () => {
+    // Show the sidebar (Responsive view)
+    sidebarOverlay.classList.toggle('show')
+})
+
+closeBtn.addEventListener('click', () => {
+    // Hide the sidebar (Responsive view)
+    sidebarOverlay.classList.toggle('show')
+})
+
+
