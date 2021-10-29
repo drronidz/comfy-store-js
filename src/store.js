c7e584289bdacd20422603b4647817d5239acd35
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-29 16:00:08 +0100

    Comfy Store Project (John Smigla): Setting up LocalStorage ...

--- src/store.js
+++ src/store.js
@@ -2,13 +2,14 @@ import { getStorageItem, setStorageItem } from './utils.js';
 
 
 
-let store = [];
+let store = getStorageItem('store');
 const setupStore = (products) => {
     store = products.map(product => {
         const {id, fields : {fetured, name, price, company, colors, image:img}} = product
         const image= img[0].thumbnails.large.url
         return {id, fetured, name, price, company, colors, image}
     })
+    setStorageItem('store', store)
 };
 console.log(store)
 const findProduct = () => {};
